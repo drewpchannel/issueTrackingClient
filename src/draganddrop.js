8f0fb1d2c2b2b7ffaa49fc577c528de26afbcf81
drewpchannel <drewpcchannel@gmail.com>
2023-03-28 10:27:32 -0400

    updating tickets from db on load

--- src/draganddrop.js
+++ src/draganddrop.js
@@ -9,32 +9,62 @@ class FileZone extends React.Component {
       tickets: [
         { id: 0, subject: "something 1", body: "this email message 1"},
         { id: 1, subject: "something 2", body: "this email message 2"},
-      ]
+      ],
+      someSubjectField : '',
+      someBodyField : '',
     }
   }
 
-  onDragOver = (e) => {
-    e.dataTransfer.effectAllowed = "all";
-    e.stopPropagation();
-    e.preventDefault();
+  getSavedTickets(username) {
+    console.log('trying to fetch');
+    fetch(`http://${serverIp}:3001/ticketCheck`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json',},
+      body: JSON.stringify({"user": [{username: document.cookie.split('=')[1]}]})
+    })
+    .then(response => response.json(),
+    error => console.log(error)
+    )
+    .then(response => {
+      console.log('orig tickets')
+      console.log(this.state.tickets)
+      this.setState({tickets: response.dbRes});
+      console.log('updated tickets')
+      console.log(this.state.tickets);
+    })
   }
 
-  onDragEnter = (e) => {
-    e.dataTransfer.setData("text/plain", e.target.id);
-    e.dataTransfer.effectAllowed = "all";
-    e.stopPropagation();
-    e.preventDefault();
+  componentDidMount() {
+    this.getSavedTickets(document.cookie.split('=')[1]);
   }
 
-  onFileDrop = (e) => {
+  setSubject(e) {
+    this.setState({someSubjectField: e});
+  }
+
+  setBody(e) {
+    this.setState({someBodyField: e});
+  }
+
+  onTicketSubmit = (e) => {
     e.stopPropagation();
     e.preventDefault();
+    
+    let ticketInfo = {
+      "ticket": [
+        {
+          "subject" : this.state.someSubjectField,
+          "body": this.state.someBodyField
+        }
+      ]
+    }
 
     fetch(`http://${serverIp}:3001/emailDrop`, {
       method: 'POST',
       mode: 'cors',
       headers: { 'Content-Type': 'application/json',},
-      body: JSON.stringify({"user": [{username: 'drew'}]})
+      body: JSON.stringify({"user": [{username: document.cookie.split('=')[1], file: ticketInfo}]})
     })
     .then(response => response.json(),
     error => console.log(error)
@@ -42,56 +72,30 @@ class FileZone extends React.Component {
     .then(response => {
       console.log(response);
     })
-
-    //var file = e.dataTransfer.files[0],
-    //    reader = new FileReader();
-    /*
-    reader.onload = function(event) {
-      let fileString = String(event.target.result);
-      let fileArray = fileString.split('');
-      let newString;
-      fileArray.forEach((index => {
-        if (/([A-Za-z\r\n !@#$%^&*():,.])/.test(index)) {
-          newString = newString + index;
-        }
-      }));
-      let firstSubjInd = newString.match("Subject: ");
-      newString = newString.substr(firstSubjInd.index + 9);
-      //if forwards, not working 100% yet but may be in future
-
-      //if (secondSubjInd) {
-      //  newString = newString.substr(secondSubjInd.index + 9);
-      //}
-
-      //find new line where subject ends
-
-      const subjEndInd = newString.match(/\n/);
-      console.log('Subject: ' + newString.substr(0, subjEndInd.index));
-      console.log('Body Preview: ' + newString.substr(subjEndInd.index + 5, 300));
-
-      let newTickets = this.state.tickets;
-      newTickets.push({
-        id: newTickets.length,
-        subject: newString.substr(0, subjEndInd.index),
-        body: newString.substr(subjEndInd.index + 5, 300)
-      }) 
-      this.setState({tickets: newTickets});
-    }.bind(this);
-    
-    reader.readAsText(file);
-    */
   }
 
   render() {
     return (
-      <div
-        onDragEnter={this.onDragEnter}
-        onDragOver={this.onDragOver}
-        onDrop={this.onFileDrop}>
-        Drag and drop file here
+      <div>
+        <form onSubmit={this.onTicketSubmit}>
+          <label>Enter Subjet Here:
+            <input 
+              type="text" 
+              onChange={(e) => this.setSubject(e.target.value)}
+            />
+          </label>
+          <label>Enter Body Here:
+            <input 
+              type="text" 
+              onChange={(e) => this.setBody(e.target.value)}
+            />
+          </label>
+          <input type="submit" />
+        </form>
+
         {this.state.tickets.map((tickets) => {
           return <li key={tickets.id}>
-            {tickets.subject} {tickets.body}
+            {tickets.asubject} {tickets.abody}
           </li>
         })}
       </div>)
